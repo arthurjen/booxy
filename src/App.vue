eabea0eefb60c30a5848222eff5860baf9335ed2
Arthur Jen <arthurljen@gmail.com>
2018-08-13 10:06:09 -0700

    renamed screenshot

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,32 @@
 <template>
   <div id="app">
-    <h1>booxy</h1>
-    <section id="box-container">
-      <Boox/>
-    </section>
+    <nav class="main-nav">
+      <router-link to="/">Home</router-link>
+      <br>
+      <router-link to="/booxy">Booxy</router-link>
+      <br>
+      <router-link to="/scootsy">Scootsy</router-link>
+    </nav>
+    <router-view class="content"/>
   </div>
 </template>
 
 <script>
-import Boox from './components/Boox'
-
 export default {
-  components: {
-    Boox
-  }
+  
 }
 </script>
 
 <style>
 #app {
+  display: grid;
+  grid-template: auto / 200px auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+} 
 
-h1 {
-  position: absolute;
-  top: 47%;
-  color: white;
-  font-size: 1em;
+.main-nav {
+  border-right: 1px solid lightgrey;
 }
-
-#box-container {
-  width: 800px;
-  height: 800px;
-}
-
 
 </style>
---
+++ src/components/Booxy.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="booxy">
+    <h1 v-if="showTitle">booxy</h1>
+    <section id="box-container" @click="showTitle = false">
+      <Boox/>
+    </section>
+  </div>
+</template>
+
+<script>
+import Boox from './Boox'
+
+export default {
+  data() {
+    return {
+      showTitle: true
+    }
+  },
+  components: {
+    Boox
+  }
+}
+</script>
+
+<style scoped>
+#booxy {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  position: absolute;
+  color: white;
+  top: 47%;
+  font-size: 1em;
+}
+
+#box-container {
+  width: 800px;
+  height: 800px;
+}
+
+
+</style>

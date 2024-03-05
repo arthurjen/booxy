0439957e8605ba98ed9949395b2c2902475f241e
Arthur Jen <arthur.jen@commsor.com>
2024-03-05 13:16:02 -0800

    test docs 2

--- src/components/BooxItem.vue
+++ src/components/BooxItem.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const props = defineProps(['color'])
-const hide = ref(false);
+  import { ref } from 'vue';
+  const props = defineProps(['color'])
+  const hide = ref(false);
 
 
-function changeColor(color: string) {
-  const increments = [-30, 30];
+  function changeColor(color: string) {
+    const increments = [-30, 30];
 
-  const [red ,green, blue] = color.slice(4,-1).split(',').map(n => {
-    const toIncrement = increments[Math.floor(Math.random() * increments.length)];
-    return Math.abs(Number(n) + toIncrement);
-  });
+    const [red ,green, blue] = color.slice(4,-1).split(',').map(n => {
+      const toIncrement = increments[Math.floor(Math.random() * increments.length)];
+      return Math.abs(Number(n) + toIncrement);
+    });
 
-  return `rgb(${red},${green},${blue})`
-}
+    return `rgb(${red},${green},${blue})`
+  }
 
-const newColor = changeColor(props.color);
-const myStyle = ref({ backgroundColor: newColor });
+  const newColor = changeColor(props.color);
+  const myStyle = ref({ backgroundColor: newColor });
 
-function updateClicked() {
-  hide.value = true;
-  myStyle.value = { backgroundColor: "white" };
-}
+  function updateClicked() {
+    hide.value = true;
+    myStyle.value = { backgroundColor: "white" };
+  }
 </script>
 
 <template>
